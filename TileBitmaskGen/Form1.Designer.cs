c6d5346de70d2d072f1a524d272baf96e6066d7d
Mattia Fontana <mattia-fontana@outlook.com>
2025-06-13 14:23:22 +0200

    Implement CreateRulePanel method for dynamic tile rule UI in Form1

--- TileBitmaskGen/Form1.Designer.cs
+++ TileBitmaskGen/Form1.Designer.cs
@@ -28,18 +28,189 @@
         /// </summary>
         private void InitializeComponent( )
         {
+            JsonPath = new TextBox( );
+            loadJsonbtn = new Button( );
+            label1 = new Label( );
+            browseBtn = new Button( );
+            comboBoxOutType = new ComboBox( );
+            label2 = new Label( );
+            label3 = new Label( );
+            OutputPath = new TextBox( );
+            button1 = new Button( );
+            label4 = new Label( );
+            textBox1 = new TextBox( );
+            Rules = new GroupBox( );
+            panelRules = new Panel( );
+            panel1 = new Panel( );
+            Rules.SuspendLayout( );
+            panelRules.SuspendLayout( );
             SuspendLayout( );
+            // 
+            // JsonPath
+            // 
+            JsonPath.Location = new Point(103, 32);
+            JsonPath.Name = "JsonPath";
+            JsonPath.Size = new Size(719, 23);
+            JsonPath.TabIndex = 0;
+            // 
+            // loadJsonbtn
+            // 
+            loadJsonbtn.Location = new Point(909, 32);
+            loadJsonbtn.Name = "loadJsonbtn";
+            loadJsonbtn.Size = new Size(67, 23);
+            loadJsonbtn.TabIndex = 1;
+            loadJsonbtn.Text = "LoadJson";
+            loadJsonbtn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(26, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Path Json";
+            // 
+            // browseBtn
+            // 
+            browseBtn.Location = new Point(828, 31);
+            browseBtn.Name = "browseBtn";
+            browseBtn.Size = new Size(75, 24);
+            browseBtn.TabIndex = 3;
+            browseBtn.Text = "Browse";
+            browseBtn.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxOutType
+            // 
+            comboBoxOutType.FormattingEnabled = true;
+            comboBoxOutType.Items.AddRange(new object[] { "CSharp", "Java", "Cpp" });
+            comboBoxOutType.Location = new Point(103, 66);
+            comboBoxOutType.Name = "comboBoxOutType";
+            comboBoxOutType.Size = new Size(121, 23);
+            comboBoxOutType.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(26, 69);
+            label2.Name = "label2";
+            label2.Size = new Size(71, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Output type";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(26, 103);
+            label3.Name = "label3";
+            label3.Size = new Size(72, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Output Path";
+            // 
+            // OutputPath
+            // 
+            OutputPath.Location = new Point(103, 100);
+            OutputPath.Name = "OutputPath";
+            OutputPath.Size = new Size(719, 23);
+            OutputPath.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(828, 99);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 24);
+            button1.TabIndex = 8;
+            button1.Text = "Browse";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(26, 138);
+            label4.Name = "label4";
+            label4.Size = new Size(101, 15);
+            label4.TabIndex = 9;
+            label4.Text = "Default Tile Name";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(133, 135);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(306, 23);
+            textBox1.TabIndex = 10;
+            // 
+            // Rules
+            // 
+            Rules.Controls.Add(panelRules);
+            Rules.Location = new Point(26, 208);
+            Rules.Name = "Rules";
+            Rules.Size = new Size(950, 514);
+            Rules.TabIndex = 11;
+            Rules.TabStop = false;
+            Rules.Text = "Rules";
+            // 
+            // panelRules
+            // 
+            panelRules.AutoScroll = true;
+            panelRules.Controls.Add(panel1);
+            panelRules.Location = new Point(6, 22);
+            panelRules.Name = "panelRules";
+            panelRules.Size = new Size(938, 475);
+            panelRules.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(932, 141);
+            panel1.TabIndex = 0;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1348, 586);
+            ClientSize = new Size(988, 734);
+            Controls.Add(Rules);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(button1);
+            Controls.Add(OutputPath);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(comboBoxOutType);
+            Controls.Add(browseBtn);
+            Controls.Add(label1);
+            Controls.Add(loadJsonbtn);
+            Controls.Add(JsonPath);
             Name = "Form1";
             Text = "Form1";
+            Rules.ResumeLayout(false);
+            panelRules.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout( );
         }
 
         #endregion
+
+        private TextBox JsonPath;
+        private Button loadJsonbtn;
+        private Label label1;
+        private Button browseBtn;
+        private ComboBox comboBoxOutType;
+        private Label label2;
+        private Label label3;
+        private TextBox OutputPath;
+        private Button button1;
+        private Label label4;
+        private TextBox textBox1;
+        private GroupBox Rules;
+        private Panel panelRules;
+        private Panel panel1;
+
+
+
+
+
+
     }
 }
--- TileBitmaskGen/Form1.cs
+++ TileBitmaskGen/Form1.cs
@@ -6,5 +6,103 @@ namespace TileBitmaskGen
         {
             InitializeComponent();
         }
+
+        private Panel CreateRulePanel(int ruleIndex)
+        {
+            // Pseudocode:
+            // 1. Create a new fixed-size Panel.
+            // 2. Add a TextBox for the tile name, with a label "Tile Name".
+            // 3. Create a 3x3 grid of ComboBox (or other control) to select "None", "Any", "Same" for each cell.
+            // 4. Add descriptive labels for the grid (e.g. "Neighbors", "Center", etc).
+            // 5. Return the panel.
+
+            var panel = new Panel
+            {
+                Size = new Size(400, 180),
+                BorderStyle = BorderStyle.FixedSingle,
+                Margin = new Padding(5)
+            };
+
+            // Label for tile name
+            var labelTileName = new Label
+            {
+                Text = "Tile Name:",
+                Location = new Point(10, 10),
+                AutoSize = true
+            };
+            panel.Controls.Add(labelTileName);
+
+            // TextBox for tile name
+            var textBoxTileName = new TextBox
+            {
+                Name = $"textBoxTileName_{ruleIndex}",
+                Location = new Point(80, 7),
+                Size = new Size(120, 23)
+            };
+            panel.Controls.Add(textBoxTileName);
+
+            // Label for grid
+            var labelGrid = new Label
+            {
+                Text = "Neighbor pattern:",
+                Location = new Point(10, 40),
+                AutoSize = true
+            };
+            panel.Controls.Add(labelGrid);
+
+            // Options for neighbors
+            string[] options = { "None", "Any", "Same" };
+
+            // 3x3 grid of ComboBox
+            int startX = 30;
+            int startY = 65;
+            int cellSize = 40;
+            ComboBox[,] gridBoxes = new ComboBox[3, 3];
+
+            for (int row = 0 ; row < 3 ; row++)
+            {
+                for (int col = 0 ; col < 3 ; col++)
+                {
+                    var combo = new ComboBox
+                    {
+                        Name = $"combo_{ruleIndex}_{row}_{col}",
+                        Location = new Point(startX + col * cellSize, startY + row * cellSize),
+                        Size = new Size(cellSize, 23),
+                        DropDownStyle = ComboBoxStyle.DropDownList
+                    };
+                    combo.Items.AddRange(options);
+                    combo.SelectedIndex = (row == 1 && col == 1) ? 2 : 0; // Center default "Same", others "None"
+                    gridBoxes[row, col] = combo;
+                    panel.Controls.Add(combo);
+                }
+            }
+
+            // Descriptive labels for directions
+            var directions = new[] { "Top", "Center", "Bottom" };
+            for (int row = 0 ; row < 3 ; row++)
+            {
+                var lbl = new Label
+                {
+                    Text = directions[row],
+                    Location = new Point(startX + 3 * cellSize + 5, startY + row * cellSize + 3),
+                    AutoSize = true
+                };
+                panel.Controls.Add(lbl);
+            }
+            var directionsCol = new[] { "Left", "Center", "Right" };
+            for (int col = 0 ; col < 3 ; col++)
+            {
+                var lbl = new Label
+                {
+                    Text = directionsCol[col],
+                    Location = new Point(startX + col * cellSize, startY - 18),
+                    AutoSize = true
+                };
+                panel.Controls.Add(lbl);
+            }
+
+            return panel;
+        }
+
     }
 }
